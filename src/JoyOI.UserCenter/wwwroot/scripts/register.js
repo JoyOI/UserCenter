d5dc2ae9248939dc9a36d8d7373528bb4de83b01
雨宮丶優子 <911574351@qq.com>
2017-05-07 12:12:22 +0800

    Implement register first page

---
+++ src/JoyOI.UserCenter/wwwroot/scripts/register.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('.textbox').focus(function () {
+        $(this).prev().addClass('active');
+    });
+    $('.textbox').blur(function () {
+        $(this).prev().removeClass('active');
+    });
+});
