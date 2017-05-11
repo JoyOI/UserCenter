cfdc10f13a5e4430f929569cf08608232d7c4e40
雨宮丶優子 <911574351@qq.com>
2017-05-11 21:15:59 +0800

    Implement uc sdk

--- src/JoyOI.UserCenter.SDK/UserCenter.cs
+++ src/JoyOI.UserCenter.SDK/UserCenter.cs
@@ -8,19 +8,104 @@ namespace JoyOI.UserCenter.SDK
 {
     public class UserCenter
     {
+        private Guid _appId;
+        private string _secret;
+
         private static Uri BaseUri = new Uri("http://api.uc.joyoi.com");
 
         public async Task<ResponseBody<User>> AuthorizeAsync(string username, string password)
         {
             using (var client = new HttpClient() { BaseAddress = BaseUri })
             {
-                var result = await client.PostAsync("/Authorize", new FormUrlEncodedContent(new Dictionary<string, string>()
+                var result = await client.PostAsync("/Authorize/" + _appId, new FormUrlEncodedContent(new Dictionary<string, string>()
                 {
                     { "username", username },
                     { "password", password }
                 }));
                 var ret = await result.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<ResponseBody<User>>(ret);
+            }
+        }
+
+        public async Task<ResponseBody<long>> GetExtensionCoin(
+            Guid openId,
+            string accessToken,
+            string field)
+        {
+            using (var client = new HttpClient() { BaseAddress = BaseUri })
+            {
+                var result = await client.PostAsync("/GetExtensionCoin/" + _appId, new FormUrlEncodedContent(new Dictionary<string, string>()
+                {
+                    { "secret", _secret },
+                    { "openid", openId.ToString() },
+                    { "accessToken", accessToken },
+                    { "field", field }
+                }));
+                var ret = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ResponseBody<long>>(ret);
+            }
+        }
+
+        public async Task<ResponseBody<long>> SetExtensionCoin(
+            Guid openId,
+            string accessToken,
+            string field,
+            long value)
+        {
+            using (var client = new HttpClient() { BaseAddress = BaseUri })
+            {
+                var result = await client.PostAsync("/SetExtensionCoin/" + _appId, new FormUrlEncodedContent(new Dictionary<string, string>()
+                {
+                    { "secret", _secret },
+                    { "openid", openId.ToString() },
+                    { "accessToken", accessToken },
+                    { "field", field },
+                    { "value", value.ToString() }
+                }));
+                var ret = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ResponseBody<long>>(ret);
+            }
+        }
+
+        public async Task<ResponseBody<long>> IncreaseExtensionCoin(
+            Guid openId,
+            string accessToken,
+            string field,
+            long value)
+        {
+            using (var client = new HttpClient() { BaseAddress = BaseUri })
+            {
+                var result = await client.PostAsync("/IncreaseExtensionCoin/" + _appId, new FormUrlEncodedContent(new Dictionary<string, string>()
+                {
+                    { "secret", _secret },
+                    { "openid", openId.ToString() },
+                    { "accessToken", accessToken },
+                    { "field", field },
+                    { "value", value.ToString() }
+                }));
+                var ret = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ResponseBody<long>>(ret);
+            }
+        }
+
+        public async Task<ResponseBody<long>> DecreaseExtensionCoin(
+            Guid openId,
+            string accessToken,
+            string field,
+            long value)
+        {
+            using (var client = new HttpClient() { BaseAddress = BaseUri })
+            {
+                var result = await client.PostAsync("/DecreaseExtensionCoin/" + _appId, new FormUrlEncodedContent(new Dictionary<string, string>()
+                {
+                    { "secret", _secret },
+                    { "openid", openId.ToString() },
+                    { "accessToken", accessToken },
+                    { "field", field },
+                    { "value", value.ToString() }
+                }));
+                var ret = await result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<ResponseBody<long>>(ret);
             }
         }
     }
