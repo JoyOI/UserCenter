ca2b5ac3c382addf77966544b52c0c1ed1d7e105
あまみや ゆうこ <yiszheng@microsoft.com>
2017-06-26 22:40:48 +0800

    Implement profile page

---
+++ src/JoyOI.UserCenter/wwwroot/scripts/joyoi.extend.js
@@ -0,0 +1,19 @@
+﻿function changeAvatarSource()
+{
+    $('.avatar-section').hide();
+    var src = $('#lstAvatarResources').val();
+    if (src === 'LocalStorage') {
+        $('#uploadAvatar').show();
+    } else if (src === 'WeChatPolling') {
+        $('#wechatAvatar').show();
+    } else {
+        $('#gravatar').show();
+    }
+}
+
+$(document).ready(function () {
+    changeAvatarSource();
+    $('#lstAvatarResources').change(function () {
+        changeAvatarSource();
+    });
+});
